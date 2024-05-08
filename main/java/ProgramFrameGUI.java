fd9c44d6cbe5371cdb643af8a7a4517a07b9caad
kupkao <163822558+kupkao@users.noreply.github.com>
2024-05-08 22:46:09 +0200

    Add files via upload

---
+++ main/java/ProgramFrameGUI.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class ProgramFrameGUI extends JFrame {
+    public ProgramFrameGUI (){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(1400,1000);
+        setTitle("Semafor");
+        setResizable(false);
+        ProgramPanelGUI programPanelGUI = new ProgramPanelGUI();
+        add(programPanelGUI);
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+}
